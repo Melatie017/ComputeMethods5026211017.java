b96e149ec0a20d5ac04f47139af9254fd72c924d
Melatie017 <100182917+Melatie017@users.noreply.github.com>
2022-03-01 12:39:43 +0700

    Create ComputeMethods5026211017.java

---
+++ ComputeMethods5026211017.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class ComputeMethods5026211017{
+
+	//temperature
+	public static double fToC(double degreesF){
+		double suhuF = 5/9*(degreesF-32);
+		return suhuF;
+}
+
+	//hypotenuse
+	public static double hypotenuse(int a, int b){
+		return Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+}
+
+	//roll dice
+	public static int roll(){
+	 	Random dice = new Random();
+		int radice = dice.nextInt(6)+1;
+		return radice;
+
+	}
+}
